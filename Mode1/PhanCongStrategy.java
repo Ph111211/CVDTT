0944832466cb6bcaeb2fb3272c0440c779c75ddd
Ph11 <phongphu7015@gmail.com>
2025-06-21 10:27:26 +0700

    updated

--- Mode1/PhanCongStrategy.java
+++
@@ -1,4 +0,0 @@
-import java.util.List;
-public interface PhanCongStrategy {
-    void phanCong(List<GiangVien> giangViens, List<SinhVienDoAn> sinhViens);
-}
--- Mode2/ChonGVStrategy.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface ChonGVStrategy {
-    void chonGiangVien(SinhVien sv, List<GiangVien> dsGiangVien);
-    
-} 
