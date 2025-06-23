6d882546db26e4afb622fa516146d35f50cf3346
Ph11 <phongphu7015@gmail.com>
2025-06-23 09:00:50 +0700

    repush

--- 2251172452_PhamDucPhu/ChonGiangVienHuongDan/strategy/ChonGVStrategy.java
+++
@@ -1,9 +0,0 @@
-package strategy;
-
-import model.SinhVien;
-import model.GiangVien;
-import java.util.List;
-
-public interface ChonGVStrategy {
-    void chonGiangVien(SinhVien sv, List<GiangVien> dsGV);
-}
